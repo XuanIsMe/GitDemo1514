70fe51ebcc86e2236f8caf7711aa711fe310338d
lzxuan <421051332@qq.com>
2015-11-11 15:10:17 +0800

    first commit pro

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by lzxuan on 15/11/11.
+//  Copyright (c) 2015年 lzxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
